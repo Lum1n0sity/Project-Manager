b8baa2c2b2898f96511625a39680608c42ad6ecc
Raphael_Laptop <raphael221@outlook.de>
2023-08-01 12:12:19 +0200

    Account UI done - State01_08_2023

--- renderer/account-ui.js
+++ renderer/account-ui.js
@@ -1,27 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('login_container');
+    const container_lo = document.getElementById('login_container');
+    const container_cr = document.getElementById('create_acc_container');
+
     const acc_Button = document.getElementById('Account');
+    const switch_lo = document.getElementById('switch_login_cr_btn');
+    const switch_cr = document.getElementById('switch_create_lo_btn');
 
-    
-
-    const close_Button = document.getElementById('close_btn');
+    const close_Button_lo = document.getElementById('close_btn_lo');
+    const close_Button_cr = document.getElementById('close_btn_cr');
 
     var isAccMenuOpen = false;
 
     acc_Button.addEventListener('click', function () {
-        container.style.display = 'block';
-        close_Button.style.display = 'block';
+        container_lo.style.display = 'block';
+        close_Button_lo.style.display = 'block';
         isAccMenuOpen = true;
     });
 
-    close_Button.addEventListener('click', function () {
+    close_Button_lo.addEventListener('click', function () {
         if (isAccMenuOpen)
         {
-            container.style.display = 'none'
-            close_Button.style.display = 'none'
+            container_lo.style.display = 'none';
+            close_Button_lo.style.display = 'none';
             isAccMenuOpen = false;
         }
     });
 
+    close_Button_cr.addEventListener('click', function () {
+        if (isAccMenuOpen)
+        {
+            container_lo.style.display = 'none';
+            container_cr.style.display = 'none';
+            close_Button_cr.style.display = 'none';
+            close_Button_lo.style.display = 'none';
+        }
+    });
 
+    switch_cr.addEventListener('click', function () {
+        if (isAccMenuOpen)
+        {
+            container_cr.style.display = 'block';
+            container_lo.style.display = 'none';
+            close_Button_lo.style.display = 'none';
+            close_Button_cr.style.display = 'block';
+        }
+    });
+
+    switch_lo.addEventListener('click', function () {
+        if (isAccMenuOpen)
+        {
+            container_lo.style.display = 'block';
+            container_cr.style.display = 'none';
+            close_Button_cr.style.display = 'none';
+            close_Button_lo.style.display = 'block';
+        }
+    });
 })
